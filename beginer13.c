03cc6468ce04a0c19889f2171b58a04bc057135c
ABISHEK <abishekperiyasamy@gmail.com>
2017-09-12 22:10:34 +0530

    Create beginer13.c

---
+++ beginer13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,n,count=0;
+scanf("%d",&a);
+for(n=2;n<a;n++)
+{
+if(a%n==0)
+count++;
+}
+if(count==0)
+printf("prime number\n");
+else
+printf("not a prime number\n");
+}

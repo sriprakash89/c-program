09a51a518c1a2a68d30f68097ba9053fb25cb78f
ABISHEK <abishekperiyasamy@gmail.com>
2018-01-10 16:32:43 +0530

    Update beginner5.c

--- beginner/beginner5.c
+++ beginner/beginner5.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-void main()
+int main()
 {
 int a,b,c;
-scanf("%d%d%d,&a,&b,&c);
+scanf("%d%d%d",&a,&b,&c);
 if((a>b)&&(a>c))
 printf("a is greater");
-elseif((b>a)&&(b>c))
+else if((b>a)&&(b>c))
 printf("b is greater");
 else
 printf("c is greater");

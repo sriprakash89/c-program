a6c05e707ef3edbb29c23cd4c6ab98727d2cf101
ABISHEK <abishekperiyasamy@gmail.com>
2018-01-10 16:58:14 +0530

    Update beginner9.c

--- beginner/beginner9.c
+++ beginner/beginner9.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
-void main()
+int main()
 {
-int a,i,sum=0;
+int a[10],i,c,b,sum=0;
 printf("enter the limit of natural numbers");
-scanf("%d",&a);
-for(i=0;i<=a;i++)
+printf("  \nenter upto what u have to sum ");
+scanf("%d%d",&c,&b);
+for(i=1;i<=c;i++)
+{
+ scanf("%d",&a[i]);   
+}
+for(i=0;i<=b;i++)
+{
 sum=sum+i;
+}
 printf("%d",sum);
 }

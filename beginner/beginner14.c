1bc05ce9ffda60058c68cc3223491abdcc3c3632
ABISHEK <abishekperiyasamy@gmail.com>
2018-01-19 15:48:10 +0530

    Update beginner14.c

--- beginner/beginner14.c
+++ beginner/beginner14.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
-void main()
+int main()
 {
 int a,b,i;
 printf("enter the two limits");
-scanf("%d%d",a,b);
+scanf("%d%d",&a,&b);
 for(i=a;i<b;i++)
 {
-if(i%2==0)
-continue;
-else
+if((i+1)%2==0)
 printf("%d\n",i);
 }
 }

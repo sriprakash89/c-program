42d86d0dd1861b1cdcc725c06f5c4194a952ebb8
ABISHEK <abishekperiyasamy@gmail.com>
2018-01-19 15:59:13 +0530

    Update beginner16.c

--- beginner/beginner16.c
+++ beginner/beginner16.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
-void main()
+int main()
 {
-int a,b,i,count;
+int n,a,b,i,count;
 printf("enter the two limits");
-scanf("%d%d",a,b);
+scanf("%d%d",&a,&b);
 for(i=a;i<b;i++)
 {
 count=0;
-for(int n=2;n<i;n++)
+for(n=2;n<i;n++)
 {
 if(i%n==0)
 count++;
 }
 if(count==0)
-printf("prime number\n");
-else
-printf("not a prime number\n");
+printf("%d\n",i);
 }
 }
-

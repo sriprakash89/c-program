0d96513cc462b0b0f8afe4e7f657fec7724a8096
ABISHEK <abishekperiyasamy@gmail.com>
2018-01-21 05:27:13 +0000

    Update beginner25.c

--- beginner/beginner25.c
+++ beginner/beginner25.c
@@ -1,11 +1,26 @@
-#include<stido.h>
-void main()
+#include<stdio.h>
+int main()
 {
-char a;
-int i;
-a=i;
-for(i=0;i<=255;i++)
-{
-printf("%c\n",a);
-}
+   int n,a[100],i,temp,j,k;
+   printf("enter the count\nenter the elements");
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(j=1;j<=n;j++)
+   {
+      for(k=j+1;k<=n;k++)
+      {
+        if(a[j]>=a[k])
+        {
+        temp=a[j];
+        a[j]=a[k];
+        a[k]=temp;
+            
+        }
+      }
+   
+   }
+     printf(" %d\t",a[j/2]);
 }

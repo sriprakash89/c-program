649cc4248943e19a01227b042c25f45d7f7f02d9
ABISHEK <abishekperiyasamy@gmail.com>
2018-01-31 15:56:29 +0530

    Update beginner33.c

--- beginner/beginner33.c
+++ beginner/beginner33.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main() {
-char a[100];
+char c[100];
 int b=1,i;
-for(i=0;a[i]<=20;i++)
+for(i=0;i<=20;i++)
 {
 scanf("%c",&a[i]);
-if((a[i]==' '))
+if((c[i]==' '))
 b=b+1;
 }
 printf("%d",b);

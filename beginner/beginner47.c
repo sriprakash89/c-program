b2025e6d719084829c377a48eaf8ba4504a205d1
ABISHEK <abishekperiyasamy@gmail.com>
2018-02-07 15:43:54 +0530

    Create beginner47.c

---
+++ beginner/beginner47.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+//https://www.jdoodle.com/c-online-compiler
+int main()
+{
+    int a[100],b,j,t,i;
+    printf("enter how many numbers\nenter the numbers");
+    scanf("%d",&b);
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<b;i++)
+    {
+        for(j=i+1;j<b;j++)
+        {
+            if(a[i]>a[j])
+            {
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+            }
+        }
+        //printf("%d\n",a[i]);
+    }
+    printf("\nsmallest no. is :%d\nlargest no. is:%d",a[0],a[b-1]);
+}
+

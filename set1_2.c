5931e0fe9f7218aed4ffab0f5878eec4643c5982
sriprakash89 <47888079+sriprakash89@users.noreply.github.com>
2019-03-15 16:10:16 +0530

    Update and rename beginner/beginner1.c to set1_2.c

---
+++ set1_2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int s;
+ printf("enter the value of s");
+ scanf("%d",&s);
+ if(a%2==0)
+ {
+  printf("even");
+ }
+ else
+ {
+  printf("odd");
+ }
+ getch();
+}
